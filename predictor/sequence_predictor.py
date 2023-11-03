121a68391db49224b61c9fde6f662d64b2cc3d2d
mostafa.karimzadeh <mos.karimzadeh@gmail.com>
2023-11-03 11:39:38 +0100

    removed print(f'>expected={testy:.1f}, predicted={yhat:.1f}') from sequence_predictor.py.

--- predictor/sequence_predictor.py
+++ predictor/sequence_predictor.py
@@ -1,7 +1,7 @@
 import numpy as np
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error
 from numpy import asarray
-
 
 class UnivarientSequencePredictor:
 
@@ -25,3 +25,28 @@ class UnivarientSequencePredictor:
         model.fit(trainX, trainy)
         yhat = model.predict([testX])
         return yhat[0]
+
+    def walk_forward_validation(self, data, train, test):
+        """
+        Conducts walk forward validation, fitting the model and making predictions step-by-step.
+
+        Args:
+            data (array-like): The complete dataset.
+            train (array-like): The training dataset.
+            test (array-like): The test dataset.
+
+        Returns:
+            float: The Mean Absolute Error between predictions and actuals.
+            array-like: The actual target values.
+            list: The predicted target values.
+        """
+        predictions = list()
+        history = [x for x in train]
+        for i in range(len(test)):
+            testX, testy = test[i, :-1], test[i, -1]
+            yhat = self.univarient_predictor(history, testX)
+            predictions.append(yhat)
+            history.append(test[i])
+        error = mean_absolute_error(test[:, -1], predictions)
+        return error, test[:, -1], predictions
+

b1a0d94a954aca4b447f341d341b5974148faac1
mostafa.karimzadeh <mos.karimzadeh@gmail.com>
2023-11-07 13:38:32 +0100

    Added best_params_dict to the model.

--- predictor/run_predictor.py
+++ predictor/run_predictor.py
@@ -6,62 +6,78 @@ import sys
 import os
 import pandas as pd
 
-sys.path.append(os.path.abspath('../../'))
+sys.path.append(os.path.abspath("../../"))
 from generate_fake_data.mocked_up import run_ada
+
 
 def main():
     run_ada.ada_df_generator()
     preprocessor = DataPreprocessor(split_ratio=settings.SPLIT_RATIO)
     predictor = UnivarientSequencePredictor()
     plotter = DataPlotter()
+    walk_forward_validation_results = {}
+    col_names = ["PredictedDone", "PredictedFlow"]
+    best_params_dict = {}
 
     df = preprocessor.read_data(filename=settings.filename)
     ada_projects = preprocessor.split_and_sort(df=df)
-    ada_projects = [preprocessor.done_tickets_per_date(df=project) for project in ada_projects]
+    ada_projects = [
+        preprocessor.done_tickets_per_date(df=project) for project in ada_projects
+    ]
     ada_projects = [preprocessor.flow_per_date(df=project) for project in ada_projects]
     ada_projects = [preprocessor.filter_dataframe(project) for project in ada_projects]
-    ada_projects = [preprocessor.fill_consecutive_dates(project) for project in ada_projects]
+    ada_projects = [
+        preprocessor.fill_consecutive_dates(project) for project in ada_projects
+    ]
     plotter.plot_projects(ada_projects=ada_projects, last_n_days=None)
 
     all_transformed_dfs = []
     for col_idx in [0, 1]:
-        transformed_dfs = preprocessor.process_dataframe_series(ada_projects, col_idx, n_in=settings.N_IN,
-                                                                n_out=settings.N_OUT)
+        transformed_dfs = preprocessor.process_dataframe_series(
+            ada_projects, col_idx, n_in=settings.N_IN, n_out=settings.N_OUT
+        )
         all_transformed_dfs.append(transformed_dfs)
 
     all_train_test_splits = []
     for transformed_dfs in all_transformed_dfs:
-        train_test_splits = [preprocessor.train_test_split(data) for data in transformed_dfs]
+        train_test_splits = [
+            preprocessor.train_test_split(data) for data in transformed_dfs
+        ]
         all_train_test_splits.append(train_test_splits)
 
-    walk_forward_validation_results = {}
-    col_names = ["PredictedDone", "PredictedFlow"]
     for col_idx, train_test_splits in enumerate(all_train_test_splits):
         for df_idx, split in enumerate(train_test_splits):
             train, test = split
-            result = predictor.walk_forward_validation(train, test)
+            mae, results_df, best_params = predictor.walk_forward_validation(train, test)
             key = f"splits_{col_names[col_idx]}_df{df_idx + 1}"
-            walk_forward_validation_results[key] = result
+            walk_forward_validation_results[key] = {'MAE': mae, 'ResultsDF': results_df}
+            best_params_dict[key] = best_params
 
-    return ada_projects, all_train_test_splits, walk_forward_validation_results
+    return ada_projects, all_train_test_splits, walk_forward_validation_results, best_params_dict
+
 
 if __name__ == "__main__":
-    ada_projects, all_train_test_splits, walk_forward_validation_results = main()
+    ada_projects, all_train_test_splits, walk_forward_validation_results, best_params_dict = main()
     df1, df2, df3 = ada_projects
-    splits_col1_df1, splits_col1_df2, splits_col1_df3 = all_train_test_splits[0]
-    splits_col2_df1, splits_col2_df2, splits_col2_df3 = all_train_test_splits[1]
-    a, b = splits_col1_df1
-    result_splits_PredictedDone_df1 = walk_forward_validation_results['splits_PredictedDone_df1']
-    result_splits_PredictedDone_df2 = walk_forward_validation_results['splits_PredictedDone_df2']
-    result_splits_PredictedDone_df3 = walk_forward_validation_results['splits_PredictedDone_df3']
-    result_splits_PredictedFlow_df1 = walk_forward_validation_results['splits_PredictedFlow_df1']
-    result_splits_PredictedFlow_df2 = walk_forward_validation_results['splits_PredictedFlow_df2']
-    result_splits_PredictedFlow_df3 = walk_forward_validation_results['splits_PredictedFlow_df3']
+    splits_PredictedDone_df1, splits_PredictedDone_df2, splits_PredictedDone_df3 = all_train_test_splits[0]
+    splits_PredictedFlow_df1, splits_PredictedFlow_df2, splits_PredictedFlow_df3 = all_train_test_splits[1]
+    result_splits_PredictedDone_df1 = walk_forward_validation_results[
+        "splits_PredictedDone_df1"
+    ]
+    result_splits_PredictedDone_df2 = walk_forward_validation_results[
+        "splits_PredictedDone_df2"
+    ]
+    result_splits_PredictedDone_df3 = walk_forward_validation_results[
+        "splits_PredictedDone_df3"
+    ]
+    result_splits_PredictedFlow_df1 = walk_forward_validation_results[
+        "splits_PredictedFlow_df1"
+    ]
+    result_splits_PredictedFlow_df2 = walk_forward_validation_results[
+        "splits_PredictedFlow_df2"
+    ]
+    result_splits_PredictedFlow_df3 = walk_forward_validation_results[
+        "splits_PredictedFlow_df3"
+    ]
 
-    print("Results for splits_col1_df1:", result_splits_col1_df1)
-
-
-
-
-
-
+    print(best_params_dict)
--- predictor/sequence_predictor.py
+++ predictor/sequence_predictor.py
@@ -3,6 +3,9 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error
 from numpy import asarray
 import pandas as pd
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import TimeSeriesSplit
+
 
 class UnivarientSequencePredictor:
 
@@ -22,10 +25,14 @@ class UnivarientSequencePredictor:
         """
         train = asarray(train)
         trainX, trainy = train[:, :-1], train[:, -1]
-        model = RandomForestRegressor(n_estimators=5)
-        model.fit(trainX, trainy)
-        yhat = model.predict([testX])
-        return yhat[0]
+        param_grid = {"n_estimators": [1, 2, 3]}
+        RF = GridSearchCV(estimator=RandomForestRegressor(random_state=123), param_grid=param_grid, cv=TimeSeriesSplit(n_splits=3))
+        RF.fit(trainX, trainy)
+        best_model = RF.best_estimator_
+        yhat = best_model.predict([testX])
+        regressor_name = best_model.__class__.__name__
+
+        return yhat[0], {regressor_name: RF.best_params_}
 
     def walk_forward_validation(self,train, test):
         """
@@ -41,19 +48,21 @@ class UnivarientSequencePredictor:
         """
         predictions = list()
         actuals = list()
+        best_params_dict = {}
         history = [x for x in train]
 
         for i in range(len(test)):
             testX, testy = test[i, :-1], test[i, -1]
-            yhat = self.univarient_predictor(history, testX)
+            yhat, best_params = self.univarient_predictor(history, testX)
             predictions.append(yhat)
             actuals.append(testy)
+            best_params_dict.update(best_params)
             history.append(test[i])
 
         mean_absolute_error_value = mean_absolute_error(actuals, predictions)
 
         results_df = pd.DataFrame({'Actual': actuals, 'Predicted': predictions})
 
-        return mean_absolute_error_value, results_df
+        return mean_absolute_error_value, results_df, best_params_dict
 
 
